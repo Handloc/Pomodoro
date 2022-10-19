04decdecbefb897a6eaef0ee579206d14fb06e44
Maciej Handke <maciejhandke11@gmail.com>
2022-10-19 22:46:13 +0200

    Fix switching between timers after time is up in certain category

--- main.js
+++ main.js
@@ -6,40 +6,45 @@ const settingsContainer = document.querySelector(".settings-container");
 const acceptSettingsButton = document.querySelector(".accept-button");
 const startButton = document.querySelector(".start-stop-button");
 
-let pomodoroValue = 25;
-let shortBreakValue = 5;
-let longBreakValue = 15;
-let breakInterval = 4;
+let pomodoroDefaultValue = 25;
+let shortBreakDefaultValue = 5;
+let longBreakDefaultValue = 15;
+let breakIntervalDefaultValue = 4;
+let timeValue = pomodoroDefaultValue * 60;
 let timer;
-let timeValue = pomodoroValue * 60;
 let pomodoroTimerValue;
 let shortBreakTimerValue;
 let longBreakTimerValue;
+let isRunning = false;
+let intervalCounter = 1;
 
 const changeCategory = function () {
   categoryButton.forEach(function (button) {
-    time.textContent = `${pomodoroValue}:00`;
-
+    time.textContent = `${pomodoroDefaultValue}:00`;
+    startButton.addEventListener("click", timerButtonAction);
     button.addEventListener("click", function () {
+      startButton.addEventListener("click", timerButtonAction);
       categoryButton.forEach((button) => button.classList.remove("active"));
       button.classList.add("active");
       if (button.textContent == "Pomodoro") {
-        time.textContent = `${pomodoroValue}:00`;
-        startButton.timeValue = 33;
-        console.log("POMO");
+        time.textContent = `${String(pomodoroDefaultValue).padStart(
+          2,
+          "0"
+        )}:00`;
+        timeValue = pomodoroDefaultValue * 60;
       } else if (button.textContent == "Short break") {
-        console.log("sb");
-        time.textContent = `${shortBreakValue}:00`;
-        startButton.timeValue = 3;
+        time.textContent = `${String(shortBreakDefaultValue).padStart(
+          2,
+          "0"
+        )}:00`;
+        timeValue = shortBreakDefaultValue * 60;
       } else {
-        console.log("lb");
-        time.textContent = `${longBreakValue}:00`;
-        startButton.timeValue = 13;
+        time.textContent = `${String(longBreakDefaultValue).padStart(
+          2,
+          "0"
+        )}:00`;
+        timeValue = longBreakDefaultValue * 60;
       }
-
-      startButton.addEventListener("click", function () {
-        startButton.addEventListener("click", timerStates);
-      });
     });
   });
 };
@@ -53,37 +58,77 @@ const showSettings = function () {
 
 const acceptSettings = function () {
   acceptSettingsButton.addEventListener("click", function () {
-    pomodoroValue = document.querySelector("#pomodoro-length").value;
-    shortBreakValue = document.querySelector("#short-break-length").value;
-    longBreakValue = document.querySelector("#long-break-length").value;
-    breakInterval = document.querySelector("#long-break-interval").value;
+    pomodoroDefaultValue = document.querySelector("#pomodoro-length").value;
+    shortBreakDefaultValue = document.querySelector(
+      "#short-break-length"
+    ).value;
+    longBreakDefaultValue = document.querySelector("#long-break-length").value;
+    breakIntervalDefaultValue = document.querySelector(
+      "#long-break-interval"
+    ).value;
     categoryButton.forEach(function (button) {
-      if (button.textContent == "Pomodoro") button.classList.add("active");
-      else {
+      if (button.textContent == "Pomodoro") {
+        button.classList.add("active");
+        button.click();
+      } else {
         button.classList.remove("active");
       }
     });
     mainContainer.classList.toggle("inactive");
     settingsContainer.classList.toggle("inactive");
-    pomodoroTimerValue = pomodoroValue * 60;
-    shortBreakTimerValue = shortBreakValue * 60;
-    longBreakTimerValue = longBreakValue * 60;
+    pomodoroTimerValue = pomodoroDefaultValue * 60;
+    shortBreakTimerValue = shortBreakDefaultValue * 60;
+    longBreakTimerValue = longBreakDefaultValue * 60;
   });
 };
 
-const timerStates = function (e) {
-  console.log("TIMER START");
-  if (startButton.textContent == "START") {
+const stopTimer = function () {
+  isRunning = false;
+  startButton.textContent = "START";
+  clearInterval(timer);
+};
+
+const timerButtonAction = function () {
+  if (isRunning == false) {
+    isRunning = true;
     startButton.textContent = "STOP";
     timer = setInterval(function () {
-      let minutes = String(Math.trunc(e.timeValue / 60)).padStart(2, "0");
-      let seconds = String(Math.trunc(e.timeValue % 60)).padStart(2, "0");
+      let minutes = String(Math.trunc(timeValue / 60)).padStart(2, "0");
+      let seconds = String(Math.trunc(timeValue % 60)).padStart(2, "0");
       time.textContent = `${minutes}:${seconds}`;
       timeValue--;
+      if (timeValue <= -2) {
+        intervalCounter++;
+        categoryButton.forEach(function (button) {
+          console.log(`>>${button.textContent}
+          >>${intervalCounter}`);
+          if (intervalCounter % (breakIntervalDefaultValue * 2) == 0) {
+            if (button.textContent == "Long break") {
+              button.classList.add("active");
+              button.click();
+            } else {
+              button.classList.remove("active");
+            }
+          } else if (intervalCounter % 2 == 0) {
+            if (button.textContent == "Short break") {
+              button.classList.add("active");
+              button.click();
+            } else {
+              button.classList.remove("active");
+            }
+          } else if (intervalCounter % 1 == 0) {
+            if (button.textContent == "Pomodoro") {
+              button.classList.add("active");
+              button.click();
+            } else {
+              button.classList.remove("active");
+            }
+          }
+        });
+      }
     }, 1000);
   } else {
-    startButton.textContent = "START";
-    clearInterval(timer);
+    stopTimer();
   }
 };
 
